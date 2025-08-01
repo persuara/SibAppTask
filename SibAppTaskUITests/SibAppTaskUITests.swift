e3a731583b645fef1993357ab7997b83e755b0e7
persuara <eramabadiamir@gmail.com>
2025-08-02 03:24:24 +0330

    added UI-testing

--- SibAppTaskUITests/SibAppTaskUITests.swift
+++ SibAppTaskUITests/SibAppTaskUITests.swift
@@ -6,36 +6,42 @@
 //
 
 import XCTest
+import DataLayer
+import Logic
+import SwiftData
 
 final class SibAppTaskUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        false
+    }
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+    
+    func testOpeningAddCardSheet() {
+        let addButton = app.buttons["AddNewCard"]
+        XCTAssertTrue(addButton.waitForExistence(timeout: 3), "'Add new card' button in the toolbar does not exist.")
+        addButton.tap()
+        
+        let wordTextField = app.textFields["WordField"]
+        XCTAssertTrue(wordTextField.waitForExistence(timeout: 3))
+        wordTextField.tap()
+        wordTextField.typeText("swift")
+        
+        let meaningTextField = app.textFields["MeaningField"]
+        XCTAssertTrue(meaningTextField.waitForExistence(timeout: 3))
+        meaningTextField.tap()
+        meaningTextField.typeText("language")
+        
+        
+        let saveButton = app.buttons["Save"]
+        XCTAssertTrue(saveButton.waitForExistence(timeout: 3), "Save Button Not found")
+        saveButton.tap()
     }
 }
